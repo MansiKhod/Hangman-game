328c5ff4b2dfe21fb5cf62c72982580a2590f690
Mansi Khod <98010341+MansiKhod@users.noreply.github.com>
2024-03-13 15:47:06 +0530

    Add files via upload

---
+++ hangman/hangman_words.py
@@ -0,0 +1,11 @@
+word_list = [
+    "aardvark", "baboon", "camel", "apple", "banana", "cherry", "date",
+    "elderberry", "fig", "grape", "honeydew", "kiwi", "lemon", "mango",
+    "nectarine", "orange", "peach", "quince", "raspberry", "strawberry",
+    "tangerine", "watermelon", "avocado", "blueberry", "cantaloupe",
+    "cranberry", "dragonfruit", "elderberry", "fig", "grapefruit", "honeydew",
+    "kiwi", "lime", "mango", "nectarine", "orange", "papaya", "quince",
+    "raspberry", "strawberry", "tangerine", "ugli", "vanilla", "watermelon",
+    "yellow", "india", "japan", "korea", "malaysia", "nepal", "philippines",
+    "qatar", "russia", "singapore", "thailand", "ukraine"
+]
